9efb202a91a15f8057a80de8a59fb143ed2c8388
Tom Houlé <tom@tomhoule.com>
2020-03-25 10:04:57 +0100

    Remove cast to better exhibit database casting

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,33 @@ async fn main() {
         .await
         .unwrap();
 
-    let result = conn.prep_exec("SELECT ?", (6.4 as f32,)).await.unwrap();
+    let conn = conn
+        .drop_exec(
+            "CREATE TABLE IF NOT EXISTS float_test (id INT AUTO_INCREMENT PRIMARY KEY, num float4)",
+            (),
+        )
+        .await
+        .unwrap();
+
+    let value: f64 = 6.4;
+    let conn = conn
+        .drop_exec("INSERT INTO float_test (num) VALUES (?)", (value,))
+        .await
+        .unwrap();
+
+    let result = conn
+        .prep_exec("SELECT num FROM float_test", ())
+        .await
+        .unwrap();
 
     result
-        .map_and_drop(|row| {
+        .for_each_and_drop(|row| {
+            dbg!("new row");
             assert_eq!(
                 row.get(0),
                 Some(mysql_async::Value::Float(6.400000095367432))
-            )
+            );
+            dbg!("it's a match");
         })
         .await
         .unwrap();
